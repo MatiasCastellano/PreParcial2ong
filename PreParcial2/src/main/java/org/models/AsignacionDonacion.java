e93b7777345e6d9639ce9f0a0aac6b6109daff1c
Matias <matiascastellano93@gmail.com>
2025-06-23 17:16:30 -0300

    segundoCommit

---
+++ PreParcial2/src/main/java/org/models/AsignacionDonacion.java
@@ -0,0 +1,62 @@
+package org.models;
+
+
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+public class AsignacionDonacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "assignment_id")
+    private long idAsignacion;
+
+    @OneToOne
+    @JoinColumn(name= "donation_id")
+    private Donacion donacion;
+
+    @Column(name="notes", nullable = false)
+    private String notas;
+
+    @Column(name= "assigned_date", nullable = false)
+    private LocalDate FechaAsignacion;
+    public AsignacionDonacion(){};
+    public AsignacionDonacion(Donacion donacion1, String nota, LocalDate fecha){
+        this.donacion=donacion1;
+        this.FechaAsignacion=fecha;
+        this.notas=nota;
+    }
+
+    public long getIdAsignacion() {
+        return idAsignacion;
+    }
+
+    public void setIdAsignacion(long idAsignacion) {
+        this.idAsignacion = idAsignacion;
+    }
+
+    public Donacion getDonacion() {
+        return donacion;
+    }
+
+    public void setDonacion(Donacion donacion) {
+        this.donacion = donacion;
+    }
+
+    public String getNotas() {
+        return notas;
+    }
+
+    public void setNotas(String notas) {
+        this.notas = notas;
+    }
+
+    public LocalDate getFechaAsignacion() {
+        return FechaAsignacion;
+    }
+
+    public void setFechaAsignacion(LocalDate fechaAsignacion) {
+        FechaAsignacion = fechaAsignacion;
+    }
+}
---
+++ PreParcial2/src/main/java/org/models/Donacion.java
@@ -0,0 +1,117 @@
+package org.models;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="donation")
+
+public class Donacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "donation_id")
+    private long id;
+
+    @Column(name="donor_name", nullable = false, length = 20)
+    private String nombre;
+
+    @Column(name="donor_type", nullable = false)
+    private Tipo tipo;
+
+    @Column(name= "amount", nullable = false)
+    private double cantidad;
+
+    @Column(name= "donation_date", nullable = false)
+    private LocalDate fecha;
+
+    @Column(name="status", nullable = false)
+    private String categoria;
+
+    @Column(name="status", nullable = false)
+    private Estado estado;
+
+    @OneToOne(mappedBy = "donacion", fetch = FetchType.LAZY)
+    private AsignacionDonacion asignacion;
+
+    public enum Estado{
+        RECEIVED,
+        ASSIGNED
+    }
+
+    public enum Tipo{
+        INDIVIDUAL,
+        COMPANY
+    }
+    public Donacion(){};
+    public Donacion(String nombre, Tipo tipo1,double cant, LocalDate fechaDon,String categoria){
+        this.nombre=nombre;
+        this.tipo=tipo1;
+        this.cantidad=cant;
+        this.fecha=fechaDon;
+        this.fecha=fechaDon;
+        this.categoria=categoria;
+        this.estado= Estado.RECEIVED;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setEstado(Estado estado1){
+        this.estado=estado1;
+    }
+    public Estado getEstado(){
+        return estado;
+    }
+    public void setAsignacion(AsignacionDonacion asig){
+        this.asignacion=asig;
+    }
+    public AsignacionDonacion getAsignacion(){
+        return asignacion;
+    }
+}

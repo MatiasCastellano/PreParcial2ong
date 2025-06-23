1475f5e71a6c6700cd900dc419a7eb02286d5c3e
Matias <matiascastellano93@gmail.com>
2025-06-23 17:49:37 -0300

    termino dto

--- PreParcial2/src/main/java/org/dto/AsignacionDonacionDTO.java
+++ PreParcial2/src/main/java/org/dto/AsignacionDonacionDTO.java
@@ -1,4 +1,51 @@
 package org.dto;
 
+import org.models.AsignacionDonacion;
+
+import java.time.LocalDate;
+
 public class AsignacionDonacionDTO {
+    private long id;
+    private DonacionDTO donacion;
+    private String notas;
+    private LocalDate fechaA;
+
+
+    public AsignacionDonacionDTO(){};
+    public AsignacionDonacionDTO(DonacionDTO donacion, String nota, LocalDate fecha){
+        this.donacion=donacion;
+        this.notas=nota;
+        this.fechaA=fecha;
+    }
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public DonacionDTO getDonacion() {
+        return donacion;
+    }
+
+    public void setDonacion(DonacionDTO donacion) {
+        this.donacion = donacion;
+    }
+
+    public String getNotas() {
+        return notas;
+    }
+
+    public void setNotas(String notas) {
+        this.notas = notas;
+    }
+
+    public LocalDate getFechaA() {
+        return fechaA;
+    }
+
+    public void setFechaA(LocalDate fechaA) {
+        this.fechaA = fechaA;
+    }
 }
--- PreParcial2/src/main/java/org/dto/DonacionDTO.java
+++ PreParcial2/src/main/java/org/dto/DonacionDTO.java
@@ -13,5 +13,69 @@ public class DonacionDTO {
     private String categoria;
     private Donacion.Estado estado;
 
-    
+    public DonacionDTO(){};
+
+    public DonacionDTO(String nom, Donacion.Tipo tipo1, double cant, LocalDate fecha1, String cat, Donacion.Estado estado1){
+        this.nombre=nom;
+        this.tipo= tipo1;
+        this.cantidad=cant;
+        this.fecha=fecha1;
+        this.categoria=cat;
+        this.estado=estado1;
+    }
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Donacion.Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Donacion.Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Donacion.Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Donacion.Estado estado) {
+        this.estado = estado;
+    }
 }
